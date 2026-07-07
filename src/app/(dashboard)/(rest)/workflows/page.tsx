32faac06f633e33ba03eab67fa3b36669f456b47
enyan27 <truongdat2325@gmail.com>
2026-07-07 12:36:17 +0700

    feat: implement pagination and search functionality for workflows

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,17 +1,24 @@
 import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { HydrateClient } from "@/trpc/server";
+import type { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const Page = async () => {
-  prefetchWorkflows();
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
+  const params = await workflowsParamsLoader(searchParams);
+  prefetchWorkflows(params);
 
   return (
     <WorkflowsContainer>
       <HydrateClient>
         <ErrorBoundary fallback={<p>Error!</p>}>
-          <Suspense fallback={<p className="m-auto">Loading...</p>}>
+          <Suspense fallback={<p>Loading...</p>}>
             <WorkflowsList />
           </Suspense>
         </ErrorBoundary>
