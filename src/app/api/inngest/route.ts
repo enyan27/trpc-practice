b1fbf0d27d4bec6a052bd9572911c3e5bdff7518
enyan27 <truongdat2325@gmail.com>
2026-07-12 15:39:15 +0700

    feat: update dependencies and add workflow execution functionality

---
+++ src/app/api/inngest/route.ts
@@ -0,0 +1,9 @@
+import { inngest } from "@/inngest/client";
+import { executeWorkflow } from "@/inngest/functions";
+import { serve } from "inngest/next";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [executeWorkflow]
+});
