8871a8e15b0366f3af08f307e61705e0ee8cfa97
enyan27 <truongdat2325@gmail.com>
2026-07-15 09:53:32 +0700

    feat: add stripe trigger functionality and webhook integration

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -5,6 +5,7 @@ import { NonRetriableError } from "inngest";
 import { googleFormTriggerChannel } from "./channels/google-form-trigger";
 import { httpRequestChannel } from "./channels/http-request";
 import { manualTriggerChannel } from "./channels/manual-trigger";
+import { stripeTriggerChannel } from "./channels/stripe-trigger";
 import { inngest } from "./client";
 import { topologicalSort } from "./utils";
 
@@ -15,7 +16,7 @@ export const executeWorkflow = inngest.createFunction(
   },
   {
     event: "workflows/execute.workflow",
-    channels: [httpRequestChannel(), manualTriggerChannel(), googleFormTriggerChannel()]
+    channels: [httpRequestChannel(), manualTriggerChannel(), googleFormTriggerChannel(), stripeTriggerChannel()]
   },
   async ({ event, step, publish }) => {
     const workflowId = event.data.workflowId;
